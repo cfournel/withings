da844127f1ac40f8ec9173e4d5b49f01def448ee
= <--replace-all>
2014-07-28 15:58:59 +0200

    no message

--- lib/Withings/UserGateway.php
+++ lib/Withings/UserGateway.php
@@ -14,6 +14,19 @@ class UserGateway extends EndpointGateway{
     {
         if ( $user == null )
             throw new \Exception("No Withings User id defined");
+        return $this->makeApiRequest( 'measure?action=getbyuserid&userid=' . $user );
+    }
+    
+    /**
+     * Get user measure depending on user param ( a withings account can manage multiple users
+     *
+     * @param String $user
+     * @return Data
+     */
+    public function getMeasures( $user = null )
+    {
+        if ( $user == null )
+            throw new \Exception("No Withings User id defined");
         return $this->makeApiRequest( 'measure?action=getmeas&userid=' . $user );
     }
 }

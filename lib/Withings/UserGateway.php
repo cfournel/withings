652771077a45614e2069b038c58ffa6c3ca28746
= <--replace-all>
2014-08-18 18:10:09 +0200

    Add filter by lastUpdate to Api function

--- lib/Withings/UserGateway.php
+++ lib/Withings/UserGateway.php
@@ -5,29 +5,54 @@ namespace Withings;
 class UserGateway extends EndpointGateway{
     
     /**
-     * Get user profile depending on user param ( a withings account can manage multiple users
+     * Get user body depending on user param ( a withings account can manage multiple users
      *
      * @param String $user
      * @return Data
      */
-    public function getProfile( $user = null )
+    public function getBody( $user = null, $lastUpdate = '' )
     {
         if ( $user == null )
             throw new \Exception("No Withings User id defined");
-        return $this->makeApiRequest( 'measure?action=getbyuserid&userid=' . $user );
+        
+        if ( !empty ( $lastUpdate ) )
+            $lastUpdate = '&lastupdate=' . strtotime( $lastUpdate );
+        
+        return $this->makeApiRequest( 'measure?action=getmeas&userid=' . $user . $lastUpdate );
     }
     
     /**
-     * Get user measure depending on user param ( a withings account can manage multiple users
+     * Get user activities depending on user param ( a withings account can manage multiple users )
      *
      * @param String $user
      * @return Data
      */
-    public function getMeasures( $user = null )
+    public function getActivities( $user = null, $lastUpdate = '' )
     {
         if ( $user == null )
             throw new \Exception("No Withings User id defined");
-        return $this->makeApiRequest( 'measure?action=getmeas&userid=' . $user );
+        
+        if ( !empty ( $lastUpdate ) )
+            $lastUpdate = '&lastupdate=' . strtotime( $lastUpdate );
+        
+        return $this->makeApiRequest( 'v2/measure?action=getactivity&userid=' . $user . $lastUpdate );
+    }
+    
+    /**
+     * Get user sleeps depending on user param ( a withings account can manage multiple users )
+     *
+     * @param String $user
+     * @return Data
+     */
+    public function getSleeps( $user = null, $lastUpdate = '' )
+    {
+        if ( $user == null )
+            throw new \Exception("No Withings User id defined");
+        
+        if ( !empty ( $lastUpdate ) )
+            $lastUpdate = '&lastupdate=' . strtotime( $lastUpdate );
+        
+        return $this->makeApiRequest( 'v2/sleep?action=get&userid=' . $user . $lastUpdate );
     }
 }
 ?>
